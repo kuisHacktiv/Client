89970547b7ce91a668aa79b16be17a26382cd410
muhafaa <muhafa99@gmail.com>
2020-02-20 16:43:38 +0700

    halaman depan selesai

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex flex-column">
+    <Background class="background-sendiri" />
+    <b-card
+      class="text-center mx-auto shadow-lg bg-dewe"
+      style="width: 40%; margin-top:10%;"
+    >
+      <h2
+        class="animated tada font-weight-bold text-white"
+        style="font-family: 'Arvo', serif; font-size:5rem;"
+      >
+        Hacktiv Kuis
+      </h2>
+    </b-card>
+    <b-card
+      class="d-flex align-content-center align-items-center align-middle animated rubberBand"
+      style="width: 30%; margin: 2% auto;"
+    >
+      <div class="animated fadeIn delay-1s">
+        <h2 class="text-center font-weight-bold">Let's Get Started!</h2>
+        <b-form class="flex-column">
+          <b-form-group class="mt-3" description="Insert your name here">
+            <b-form-input
+              type="text"
+              required
+              placeholder="Name"
+            ></b-form-input>
+          </b-form-group>
+          <div class="d-flex justify-content-center">
+            <b-btn class="btn-dewe font-weight-bold" variant="primary"
+              >Start Now!</b-btn
+            >
+          </div>
+        </b-form>
+      </div>
+    </b-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Background from '../components/Background'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Background
   }
 }
 </script>
+
+<style scoped>
+.background-sendiri {
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: -10;
+}
+.bg-dewe {
+  background-color: rgba(158, 68, 68, 0.568) !important;
+  border-radius: 5rem !important;
+}
+.btn-dewe {
+  transition: 0.7s;
+}
+.btn-dewe:hover {
+  padding: 1.1rem;
+  font-size: 1.1rem;
+  font-weight: bold;
+}
+.title {
+  width: 30%;
+  margin-top: 10%;
+  padding: 0;
+}
+</style>
