704dd773b3380bb27f52ebc44a77231585e2d000
muhafaa <muhafa99@gmail.com>
2020-02-20 19:21:47 +0700

    halaman game udah

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,37 +1,68 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-3">
-        <h1 v-text="jumlahSoal"></h1>
-      </div>
-    </div>
-    <div class="row">
-      {{ randomSoal }}
-      <div class="col-10">
-        <h1>{{ randomSoal.soal }}</h1>
-      </div>
-      <h1 class="col-2" v-text="score"></h1>
-    </div>
-    <div class="row">
-      <div class="col-6">
-        <div class="btn btn-primary" v-on:click="cekJawab(true)">True</div>
-      </div>
-      <div class="col-6">
-        <div class="btn btn-danger" v-on:click="cekJawab(false)">False</div>
+  <div class="w-100 container">
+    <Background class="background-sendiri" />
+    <b-card style="margin-top:5%;">
+      <b-form>
+        <h1 class="text-center" style="font-family: 'Arvo', serif;" v-if="show">
+          {{ randomSoal.soal }}
+        </h1>
+        <div class="text-center d-flex justify-content-center" v-if="!show">
+          <b-spinner variant="primary" type="grow"></b-spinner>
+        </div>
+        <div class="d-flex justify-content-around mt-5">
+          <div>
+            <b-btn class="btn btn-primary btn-gerak" v-on:click="cekJawab(true)"
+              >True</b-btn
+            >
+          </div>
+          <div>
+            <b-btn class="btn btn-danger btn-gerak" v-on:click="cekJawab(false)"
+              >False</b-btn
+            >
+          </div>
+        </div>
+      </b-form>
+    </b-card>
+
+    <div class="d-flex justify-content-center mt-5">
+      <div class="row w-100 d-flex justify-content-around">
+        <b-card tag="article" class="col-2 mb-2 mx-1 w-100">
+          <h6 class="border-bottom text-center">Username</h6>
+          <h6 class="mt-3 text-center">{{ score }}</h6>
+        </b-card>
+
+        <b-card tag="article" class="col-2 mb-2 mx-1 w-100">
+          <h6 class="border-bottom text-center">Username</h6>
+          <h6 class="mt-3 text-center">{{ score }}</h6>
+        </b-card>
+        <b-card tag="article" class="col-2 mb-2 mx-1 w-100">
+          <h6 class="border-bottom text-center">Username</h6>
+          <h6 class="mt-3 text-center">{{ score }}</h6>
+        </b-card>
+
+        <b-card tag="article" class="col-2 mb-2 mx-1 w-100">
+          <h6 class="border-bottom text-center">Username</h6>
+          <h6 class="mt-3 text-center">{{ score }}</h6>
+        </b-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Background from '../components/Background'
 export default {
   name: 'gamePage',
+  components: {
+    Background
+  },
   data() {
     return {
       currentSoal: [],
       randomIndexnya: null,
       score: 0,
-      jumlahSoal: 0
+      jumlahSoal: 0,
+      show: true
     }
   },
   methods: {
@@ -51,12 +82,15 @@ export default {
         '<< ini jawaban asli'
       )
       if (jawaban == this.randomSoal.jawab) {
+        this.show = false
         console.log('masuk benar')
         this.score += 1
       } else if (jawaban !== this.randomSoal.jawab) {
+        this.show = false
         console.log('masuk salah')
         this.score -= 1
       }
+      this.show = true
       this.randomIndex()
     }
   },
@@ -91,4 +125,31 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.background-sendiri {
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: -10;
+}
+
+.btn-gerak {
+  transition: 0.7s;
+  padding-right: 1vw;
+  padding-left: 1vw;
+  font-size: 1.5rem;
+}
+
+.btn-gerak:hover {
+  padding: 1rem;
+  font-weight: bold;
+  font-size: 2.2rem;
+  border-radius: 50%;
+}
+
+.score {
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
